56cd000d7d4bd14d1fd533c59b3f0b50f013a872
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-26 14:03:55 -0400

    Add files via upload

---
+++ BasketBall Data Project/Services/ISeasonLAPIService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using BasketBall_Data_Project.Models.SeasonModel;
+
+namespace BasketBall_Data_Project.Services
+{
+    public interface ISeasonLAPIService
+    {
+        Task<SeasonInfo> GetSeasonInfoAsync();
+        
+    }
+}
---
+++ BasketBall Data Project/Services/SeasonLAPIService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using BasketBall_Data_Project.Models.SeasonModel;
+using System.Threading.Tasks;
+using BasketBall_Data_Project.Services;
+
+namespace BasketBall_Data_Project.Services
+{
+    public class SeasonLAPIService : ISeasonLAPIService
+    {
+        ISerializerService serializerService = new SerializerService();
+
+        public async Task<SeasonInfo> GetSeasonInfoAsync()
+        {
+            SeasonInfo season = null;
+            var client = new HttpClient(); 
+            var request = new HttpRequestMessage 
+            { 
+                Method = HttpMethod.Get, 
+                RequestUri = new Uri("https://sportscore1.p.rapidapi.com/sports/3/seasons"), 
+                Headers = 
+                { 
+                    { "x-rapidapi-key", "8aaee18d33msh73133a98dbb4908p146d53jsn17633aea599f" }, 
+                    { "x-rapidapi-host", "sportscore1.p.rapidapi.com" }, 
+                }, 
+            };
+            var response = await client.SendAsync(request);
+            if  (response.IsSuccessStatusCode)
+            {
+                season = serializerService.Deserialize<SeasonInfo>(await response.Content.ReadAsStringAsync());
+
+            }
+            return season;
+        }
+    }
+}
